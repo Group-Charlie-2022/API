4fbaf059793e3f104a4424797a179e60b79a69d2
Morgan Saville <gbt505@hotmail.com>
2022-03-03 12:16:50 +0000

    Hooked up empathiser

--- question_handler.py
+++ question_handler.py
@@ -1,10 +1,12 @@
 from conversational import Conversational
 from svm_comp import classify
 from medical import Medical
+from empathizer import empathize
 
 def answer_question(q, history):
     if classify(q) == 0:
-        return Medical.process(q, history)
+        answer = Medical.process(q, history)
+        return answer
+        # return empathize(answer)
     else:
         return Conversational.process(q, history)
-    #return f"You asked: \"{q}\"\nI do not know how to respond :("
