27cde94dc472f5157bd78e37c3d0fdd2e9ef38ac
Morgan Saville <gbt505@hotmail.com>
2022-03-03 12:15:47 +0000

    Started hooking everything up

--- question_handler.py
+++ question_handler.py
@@ -1,5 +1,10 @@
 from conversational import Conversational
+from svm_comp import classify
+from medical import Medical
 
 def answer_question(q, history):
-    return Conversational.process(q, history)
+    if classify(q) == 0:
+        return Medical.process(q, history)
+    else:
+        return Conversational.process(q, history)
     #return f"You asked: \"{q}\"\nI do not know how to respond :("
